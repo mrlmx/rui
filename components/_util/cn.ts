6359f5ee45da1cb8e3c4c4594adf63cdc596c5d1
mrlmx <906529775@qq.com>
2025-05-04 12:47:43 +0800

    feat: add cn

---
+++ components/_util/cn.ts
@@ -0,0 +1,7 @@
+// import { clsx, type ClassValue } from "clsx"
+// import { twMerge } from "tailwind-merge"
+
+// // copy from https://github.com/shadcn-ui/ui/blob/main/apps/v4/lib/utils.ts#L1-L6
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs))
+// }
