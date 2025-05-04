232562f136cd745ae43c879983a92a431d80a33f
mrlmx <906529775@qq.com>
2025-05-04 15:36:56 +0800

    feat: use webpack build umd

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    index: ["./components/index.ts"],
+  },
+  output: {
+    filename: "rui.js",
+    path: path.join(__dirname, "dist/umd"),
+    library: "Rui",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.build.json",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
